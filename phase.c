53b8cdc549c51a5267f390099c690511a1b578b8
CoupDeVent <115654669+CoupDeVent@users.noreply.github.com>
2024-11-15 15:04:50 +0100

    bug move

--- phase.c
+++ phase.c
@@ -26,6 +26,30 @@ int *nb_move_disp_phase(){
     return nb_move_disp_phase;
 }
 
+/*
+ * t_move *getRandomMoves(int N)
+{
+    int nbmoves[]={22,15,7,7,21,21,7};
+    int total_moves=100;
+    t_move *moves = (t_move *)malloc(N * sizeof(t_move));
+    for (int i = 0; i < N; i++)
+    {
+        int r = rand() % total_moves;
+        int type=0;
+        while (r >= nbmoves[type])
+        {
+            r -= nbmoves[type];
+            type++;
+        }
+        nbmoves[type]--;
+        total_moves--;
+        moves[i] = (t_move )type;
+    }
+    return moves;
+}
+ */
+
+
 int phase_tree(int nb_move_disp_phase){
 
 
